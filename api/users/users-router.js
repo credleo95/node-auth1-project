546af70eb8c8335c2dcf9d66cc3731c8d4546afe
Omarius <70237021+credleo95@users.noreply.github.com>
2021-08-14 04:12:00 -0400

    testing routes

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,7 +1,6 @@
 // Require the `restricted` middleware from `auth-middleware.js`. You will need it here!
 
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 /**
   [GET] /api/users
 
@@ -24,6 +23,9 @@ const router = express.Router();
     "message": "You shall not pass!"
   }
  */
+router.get('/', (req, res, next) => {
+  return res.json('users');
+});
 
 // Don't forget to add the router to the `exports` object so it can be required in other modules
 module.exports = router;
